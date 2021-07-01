c53a4e1d32bd90b12757310fc3df60fc1bec1936
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-07-01 16:39:26 -0600

    Update imageclassifier.py

--- service/imageclassifier.py
+++ service/imageclassifier.py
@@ -37,6 +37,7 @@ import time
 import numpy as np
 import pickle
 import os
+import warnings
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import KFold, StratifiedKFold
@@ -51,6 +52,7 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
 import joblib
 import argparse
+warnings.filterwarnings('ignore')
 #Define an arg parsers to build out a CLI
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--init", type=str) # calls installflowers17
@@ -59,74 +61,6 @@ parser.add_argument("-p", "--predict", type=str) # predict(imagearray)
 
 args = parser.parse_args()
 
-def download_dataset(filename, url, work_dir):
-	if not os.path.exists(filename):
-		print("[INFO] Downloading flowers17 dataset....")
-		filename, _ = urllib.request.urlretrieve(url + filename, filename)
-		statinfo = os.stat(filename)
-		print("[INFO] Succesfully downloaded " + filename + " " + str(statinfo.st_size) + " bytes.")
-		untar(filename, work_dir)
-
-def jpg_files(members):
-	for tarinfo in members:
-		if os.path.splitext(tarinfo.name)[1] == ".jpg":
-			yield tarinfo
-
-def untar(fname, path):
-	tar = tarfile.open(fname)
-	tar.extractall(path=path, members=jpg_files(tar))
-	tar.close()
-	print("[INFO] Dataset extracted successfully.")
-
-#-------------------------
-# MAIN FUNCTION
-#-------------------------
-def installflowers17():
-	flowers17_url  = "http://www.robots.ox.ac.uk/~vgg/data/flowers/17/"
-	flowers17_name = "17flowers.tgz"
-	train_dir      = "golem/work/dataset"
-
-	if not os.path.exists(train_dir):
-		os.makedirs(train_dir + "/train")
-
-	download_dataset(flowers17_name, flowers17_url, train_dir)
-	if os.path.exists(train_dir + "/jpg"):
-		os.rename(train_dir + "/jpg", train_dir + "/train")
-
-
-	# get the class label limit
-	class_limit = 17
-
-	# take all the images from the dataset
-	image_paths = (train_dir + "/train/*.jpg")
-
-	# variables to keep track
-	label = 0
-	i = 0
-	j = 80
-
-	# flower17 class names
-	class_names = ["daffodil", "snowdrop", "lilyvalley", "bluebell", "crocus",
-			   	   "iris", "tigerlily", "tulip", "fritillary", "sunflower", 
-			       "daisy", "coltsfoot", "dandelion", "cowslip", "buttercup",
-			       "windflower", "pansy"]
-
-	# loop over the class labels
-	for x in range(1, class_limit+1):
-		# create a folder for that class
-		os.makedirs(train_dir + "/train/" + class_names[label])
-		# get the current path
-		cur_path = train_dir + "/train/" + class_names[label] + "/"
-		# loop over the images in the dataset
-		for index, image_path in enumerate(image_paths[i:j], start=1):
-			original_path   = image_path
-			image_path      = image_path.split("/")
-			image_file_name = str(index) + ".jpg"
-			os.rename(original_path, cur_path + image_file_name)
-		
-		i += 80
-		j += 80
-		label += 1
 #------------------------------------
 # TUNEABLE PARAMETERS               |
 #------------------------------------
