0731053e166b51657a7fab1be8f246530c83ad05
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-07-09 17:09:37 -0600

    Update imageclassifier.py

--- service/imageclassifier.py
+++ service/imageclassifier.py
@@ -67,8 +67,8 @@ args = parser.parse_args()
 images_per_class = 80
 fixed_size       = tuple((500, 500))
 train_path       = "golem/work/dataset/train"
-h5_data          = 'golem/work/output/data.h5'
-h5_labels        = 'golem/work/output/labels.h5'
+h5_data          = '/golem/run/h5/data.h5'
+h5_labels        = '/golem/run/h5/labels.h5'
 bins             = 8
 num_trees = 100
 test_size = 0.10
@@ -101,96 +101,6 @@ def fd_histogram(image, mask=None):
     # return the histogram
     return hist.flatten()
 def trainmodel():
-    # get the training labels
-    train_labels = os.listdir(train_path)
-
-    # sort the training labels
-    train_labels.sort()
-    print(train_labels)
-
-    # empty lists to hold feature vectors and labels
-    global_features = []
-    labels          = []
-
-    # loop over the training data sub-folders
-    for training_name in train_labels:
-        # join the training data path and each species training folder
-        dir = os.path.join(train_path, training_name)
-
-        # get the current training label
-        current_label = training_name
-
-        # loop over the images in each sub-folder
-        for x in range(1,images_per_class+1):
-            # get the image file name
-            file = dir + "/" + str(x) + ".jpg"
-
-            # read the image and resize it to a fixed-size
-            image = cv2.imread(file)
-            image = cv2.resize(image, fixed_size)
-
-            ####################################
-            # Global Feature extraction
-            ####################################
-            fv_hu_moments = fd_hu_moments(image)
-            fv_haralick   = fd_haralick(image)
-            fv_histogram  = fd_histogram(image)
-
-            ###################################
-            # Concatenate global features
-            ###################################
-            global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
-
-            # update the list of labels and feature vectors
-            labels.append(current_label)
-            global_features.append(global_feature)
-
-        print("[STATUS] processed folder: {}".format(current_label))
-
-    print("[STATUS] completed Global Feature Extraction...")
-
-    # get the overall feature vector size
-    print("[STATUS] feature vector size {}".format(np.array(global_features).shape))
-
-    # get the overall training label size
-    print("[STATUS] training Labels {}".format(np.array(labels).shape))
-
-    # encode the target labels
-    targetNames = np.unique(labels)
-    le          = LabelEncoder()
-    target      = le.fit_transform(labels)
-    print("[STATUS] training labels encoded...")
-
-    # scale features in the range (0-1)
-    scaler            = MinMaxScaler(feature_range=(0, 1))
-    rescaled_features = scaler.fit_transform(global_features)
-    print("[STATUS] feature vector normalized...")
-
-    print("[STATUS] target labels: {}".format(target))
-    print("[STATUS] target labels shape: {}".format(target.shape))
-
-    # save the feature vector using HDF5
-    h5f_data = h5py.File(h5_data, 'w')
-    h5f_data.create_dataset('dataset_1', data=np.array(rescaled_features))
-
-    h5f_label = h5py.File(h5_labels, 'w')
-    h5f_label.create_dataset('dataset_1', data=np.array(target))
-
-    h5f_data.close()
-    h5f_label.close()
-
-    print("[STATUS] end of training..")
-
-
-    # get the training labels
-    train_labels = os.listdir(train_path)
-
-    # sort the training labels
-    train_labels.sort()
-
-    if not os.path.exists(test_path):
-        os.makedirs(test_path)
-
     # create all the machine learning models
     models = []
     models.append(('LR', LogisticRegression(random_state=seed)))
