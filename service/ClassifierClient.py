007d8307a3e46f3613d9d1762ccf539f278d4fc4
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-07-23 21:40:30 -0600

    Update ClassifierClient.py

--- service/ClassifierClient.py
+++ service/ClassifierClient.py
@@ -4,14 +4,22 @@ import argparse
 if __name__ == "__main__":
     # Initialize a Connection to the Model
     conn = rpyc.connect("localhost", 12345)
+    conn._config['sync_request_timeout'] = None
     c = conn.root
     parser = argparse.ArgumentParser()
     #Required Args to train a model
     parser.add_argument("-p", "--predict", type=str) 
     parser.add_argument("-c", "--classes", nargs='+')
-
-
+    parser.add_argument("-t", "--train", type=str)
+    parser.add_argument("-v", "--validloc", type=str)
     args = parser.parse_args()
     # do stuff over rpyc
-    prediction = c.predict(args.classes, args.predict)
-    print(args.classes[prediction[0]])
+    if args.predict != None:
+        prediction = c.predict(args.classes, args.predict)
+        print(args.classes[prediction[0]])
+    if args.train != None:
+        status = c.train(args.classes, args.train, args.validloc)
+        if status:
+            print("Model Successfully Trained")
+        else:
+            print("Error Occured in Training Model, Process did not finish")
