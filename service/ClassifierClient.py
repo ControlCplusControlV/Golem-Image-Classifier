6d2e2848c2397ef56cee4f2387418de85607960f
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-07-24 22:01:54 -0600

    Update ClassifierClient.py

--- service/ClassifierClient.py
+++ service/ClassifierClient.py
@@ -12,14 +12,21 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--classes", nargs='+')
     parser.add_argument("-t", "--train", type=str)
     parser.add_argument("-v", "--validloc", type=str)
+    parser.add_argument('--start', dest='start', action='store_true')
     args = parser.parse_args()
     # do stuff over rpyc
-    if args.predict != None:
-        prediction = c.predict(args.classes, args.predict)
-        print(args.classes[prediction[0]])
-    if args.train != None:
-        status = c.train(args.classes, args.train, args.validloc)
-        if status:
-            print("Model Successfully Trained")
+    if args.start:
+        if c.buildModel(args.train, args.validloc,args.classes):
+            print("Model Initialized Successfully")
         else:
-            print("Error Occured in Training Model, Process did not finish")
+            print("Error Initializing Model")
+    else:
+        if args.predict != None:
+            prediction = c.predict(args.classes, args.predict)
+            print(args.classes[prediction[0]])
+        if args.train != None:
+            status = c.train(args.classes, args.train, args.validloc)
+            if status:
+                print("Model Successfully Trained")
+            else:
+                print("Error Occured in Training Model, Process did not finish")
