193abd663d834f8189516c840fc4554b9bd453f2
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-08-02 20:24:09 -0700

    Update requestor.py

--- requestor.py
+++ requestor.py
@@ -8,18 +8,35 @@ import pathlib
 import random
 import string
 import sys
-
 import time
 import argparse
 from yapapi import (
     NoPaymentAccountError,
-@@ -33,33 +33,36 @@
+    __version__ as yapapi_version,
+    windows_event_loop_fix,
+)
+from yapapi import Golem
+from yapapi.services import Service, ServiceState
+
+from yapapi.log import enable_default_logger, pluralize
+from yapapi.payload import vm
+
+
+NUM_INSTANCES = 1
+STARTING_TIMEOUT = timedelta(minutes=100)
+
+
+class ImageClassifierService(Service):
+    CLASSIFIER = "/golem/run/ImageClassification.py"
+    CLASSIFIERCLIENT = "/golem/run/ClassifierClient.py"
+    @staticmethod
     async def get_payload():
         return await vm.repo(
             image_hash="5a5f3664b6f45263dc7c15a1107ad5c0ab0df3b4745c8194a8cbc8dd",
             min_mem_gib=8,
             min_storage_gib=20,
         )
+
     async def start(self):
         """
         Setup the volume so that it will play nice with the classifier and all the needed data
@@ -36,10 +53,6 @@ from yapapi import (
         zipped = yield self._ctx.commit()
         finalized = await zipped
         # Next up set up some folders in the volume so the classifier can identify it
-        self._ctx.run("/bin/ls", "/golem/work/dataset")
-        status = yield self._ctx.commit()
-        finalized = await status
-        print(finalized)
         #Now data is unzipped, next it executes
     async def run(self):
         self._ctx.run("/bin/sh", "-c", "nohup python /golem/run/ImageClassification.py run &")
@@ -87,6 +100,7 @@ from yapapi import (
                     deletion = yield self._ctx.commit()
                     ds = await deletion
                     print("Model Successfully Trained")
+
 async def main(subnet_tag, driver=None, network=None):
     async with Golem(
         budget=4.00,
@@ -94,69 +108,95 @@ async def main(subnet_tag, driver=None, network=None):
         driver=driver,
         network=network,
     ) as golem:
+
         print(
             f"yapapi version: {yapapi_version}\n"
             f"Using subnet: {subnet_tag}, "
             f"payment driver: {golem.driver}, "
             f"and network: {golem.network}\n"
         )
+
         commissioning_time = datetime.now()
+
         print(
             f"starting {pluralize(NUM_INSTANCES, 'instance')}"
         )
+
         # start the service
+
         cluster = await golem.run_service(
             ImageClassifierService,
             num_instances=NUM_INSTANCES,
             expiration=datetime.now(timezone.utc) + timedelta(minutes=120),
         )
+
         # helper functions to display / filter instances
+
         def instances():
             return [(s.provider_name, s.state.value) for s in cluster.instances]
+
         def still_running():
             return any([s for s in cluster.instances if s.is_available])
+
         def still_starting():
             return len(cluster.instances) < NUM_INSTANCES or any(
                 [s for s in cluster.instances if s.state == ServiceState.starting]
             )
+
         # wait until instances are started
+
         while still_starting() and datetime.now() < commissioning_time + STARTING_TIMEOUT:
             print(f"instances: {instances()}")
             await asyncio.sleep(5)
+
         if still_starting():
             raise Exception(f"Failed to start instances before {STARTING_TIMEOUT} elapsed :( ...")
+
         print("All instances started :)")
+
         # allow the service to run for a short while
         # (and allowing its requestor-end handlers to interact with it)
+
         start_time = datetime.now()
+
         while datetime.now() < start_time + timedelta(minutes=120): # 2 hour timeout 
             print(f"instances: {instances()}")
             await asyncio.sleep(5)
+
         print(f"stopping instances")
         cluster.stop()
+
         # wait for instances to stop
+
         cnt = 0
         while cnt < 10 and still_running():
             print(f"instances: {instances()}")
             await asyncio.sleep(5)
+
     print(f"instances: {instances()}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     now = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
     parser.set_defaults(log_file=f"simple-service-yapapi-{now}.log")
     args = parser.parse_args()
+
     # This is only required when running on Windows with Python prior to 3.8:
     windows_event_loop_fix()
+
     enable_default_logger(
         log_file=args.log_file,
         debug_activity_api=True,
         debug_market_api=True,
         debug_payment_api=True,
     )
+
     loop = asyncio.get_event_loop()
     task = loop.create_task(
         main(subnet_tag="devnet-beta.2", driver="zksync", network="rinkeby")
     )
+
     try:
         loop.run_until_complete(task)
     except NoPaymentAccountError as e:
