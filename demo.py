d4361ef8358c73134fd8f641f2a61cea3eaf5091
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-08-10 15:59:53 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,12 @@ import subprocess
 import time
 
 if __name__ == '__main__':
-    p = Popen(["python3", "requestor.py", "-d", "dataset", "-c", "dog", "monkey", "cat", "cow"], stdin=subprocess.PIPE) # Start Requestor Server
+    p = Popen(["python3", "requestor.py", "-d", "dataset", "-c", "dog", "monkey", "cat", "cow"], stdin=subprocess.PIPE) # Start Requestor Service
+    """
+    Tasks are shown already made like this, but if you are running a service it may make more sense to make each 
+    task a list like ["predict", "test1.jpg"] and concatenate them. This service also uses stdin in write, and
+    while at first communicate may seem more proper, it awaits the task to end which it never does
+    """
     tasks = ['predict test1.jpg','train train.tar.gz valid.tar.gz']
     task1 = p.stdin.write((tasks[0] + "\n").encode("utf-8"))
     task2 = p.stdin.write((tasks[1] + "\n").encode("utf-8"))
