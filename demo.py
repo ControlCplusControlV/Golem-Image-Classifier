80c144686921fc53446480d713483e06f645cb0f
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-08-22 15:38:09 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,15 +3,20 @@ import subprocess
 import time
 
 if __name__ == '__main__':
-    p = Popen(["python3", "requestor.py", "-d", "dataset", "-c", "dog", "monkey", "cat", "cow"], stdin=subprocess.PIPE) # Start Requestor Service
+    process = Popen(["python3", "requestor.py", "-d", "dataset", "-c", "dog", "monkey", "cat", "cow"], stdin=subprocess.PIPE) # Start Requestor Service
     """
     Tasks are shown already made like this, but if you are running a service it may make more sense to make each 
     task a list like ["predict", "test1.jpg"] and concatenate them. This service also uses stdin in write, and
     while at first communicate may seem more proper, it awaits the task to end which it never does
     """
     tasks = ['predict test1.jpg','train train.tar.gz valid.tar.gz']
-    task1 = p.stdin.write((tasks[0] + "\n").encode("utf-8"))
-    task2 = p.stdin.write((tasks[1] + "\n").encode("utf-8"))
+    task1 = process.stdin.write((tasks[0] + "\n").encode("utf-8"))
+    task2 = process.stdin.write((tasks[1] + "\n").encode("utf-8"))
     # Having \n on the final task, or most recent one without anothet ask coming up causes an EOF error, rather than the provider just
     # continuing on normally
-    task3 = p.stdin.write((tasks[0]).encode("utf-8"))
+    task3 = process.stdin.write((tasks[0]).encode("utf-8"))
+    # Then run you're handling of output here, tasks are queued up in stdin, in this case just wait 15 minutes
+    # So we can see the output
+    time.sleep(900)
+    # Then kill subprocess when done
+    process.kill()
